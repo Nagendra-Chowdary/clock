86a768c55056a82bfd69c37dc9350fc3fd903674
Nagendra-Chowdary <chowdarynagendra006@gmail.com>
2025-07-15 12:36:45 +0530

    Added date

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ let hrs=date.getHours();
 let mins=date.getMinutes();
 let sec=date.getSeconds();
 let millsec=date.getMilliseconds();
+let dayObj={1:"January",2:"February",3:"March",4:"April",5:"May",6:"June"}
 document.getElementsByTagName("span")[0].innerHTML=`The time is <h1>${hrs}:${mins}:${sec}:${Math.floor(millsec/100)}</h1>`;
+document.getElementsByTagName("day")[0].innerHTML=`${day} ${dayObj[month]} ${year}`;
 }
-
 setInterval(settime,100);
